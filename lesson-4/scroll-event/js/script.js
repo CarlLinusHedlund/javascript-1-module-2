88ac016f90f3e6d6f8fb1e9535538f1cfd79b69b
CarlLinusHedlund <carl.hedlund@stud.noroff.no>
2022-03-10 20:14:16 +0100

    lecture done

---
+++ lesson-4/scroll-event/js/script.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector("nav");
+function handleScroll() {
+    console.log(window.scrollY);
+
+    const scrolledY = window.scrollY;
+    
+
+    if (scrolledY > 50) {
+        document.body.classList.add("scrolled");
+        nav.classList.add("scrolled");
+    } else {
+        document.body.classList.remove("scrolled");
+        nav.classList.remove("scrolled");
+        body.classList.remoce("scrolled");
+    }
+}
+
+// window.onscroll = handleScroll;
+
+window.addEventListener("scroll", handleScroll);
